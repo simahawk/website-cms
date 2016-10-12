d7808806680c2d82088c30ac948bf412706c76f5
Simone Orsi <simahawk@gmail.com>
2016-10-12 08:49:50 +0200

    fix regression

--- website_cms/tests/test_security.py
+++ website_cms/tests/test_security.py
@@ -61,6 +61,7 @@ class TestSecurity(common.TransactionCase):
         # public user env
         env = self.env(user=self.user_public)
         model = env['cms.page']
+
         # put forbidden record in cache
         page1 = model.browse(self.page1.id)
         # this is the one we want
@@ -69,47 +70,15 @@ class TestSecurity(common.TransactionCase):
         # published page can be found
         found = len(
             model.search([('name', '=', 'Test Page 1')]))
-        self.assertEqual(found, 0)
+        self.assertEqual(found, 1)
         # not published page cannot be found
         found = len(
             model.search([('name', '=', 'Test Page 2')]))
-        self.assertEqual(found, 1)
+        self.assertEqual(found, 0)
 
         # published page can be read
         self.assertEqual(page1.website_published, True)
         # not published page cannot be read
-        with self.assertRaises(exceptions.AccessError):
-            self.assertEqual(page2.website_published, False)
-
-        # admin user can write of course
-        self.page2.website_published = True
-        # now public user can see published page
-        self.assertEqual(page2.website_published, True)
-
-        # page cannot be written
-        with self.assertRaises(exceptions.AccessError):
-            page2.website_published = False
-
-        # page cannot be deleted
-        with self.assertRaises(exceptions.AccessError):
-            page2.unlink()
-
-        # page cannot be created
-        with self.assertRaises(exceptions.AccessError):
-            model.create({'name': 'Impossible'})
-
-    def __test_public_user_access(self):
-        # public user env
-        env = self.env(user=self.user_public)
-        model = env['cms.page']
-        # put forbidden record in cache
-        page1 = model.browse(self.page1.id)
-        # this is the one we want
-        page2 = model.browse(self.page2.id)
-
-        # published page can be accessed
-        self.assertEqual(page1.website_published, True)
-        # not published page cannot be accessed
         with self.assertRaises(exceptions.AccessError):
             self.assertEqual(page2.website_published, False)
 
