a5a551ef613972c4500ba748bb432045ebeb203e
Simone Orsi <simahawk@gmail.com>
2020-04-18 18:05:16 +0200

    [MIG] cms_form: Migration to 13.0

--- cms_form/__manifest__.py
+++ cms_form/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "CMS Form",
     "summary": """
         Basic content type form""",
-    "version": "11.0.1.7.2",
+    "version": "13.0.1.0.0",
     "license": "LGPL-3",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "depends": ["website", "cms_info", "cms_status_message",],
